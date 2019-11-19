7a429a089b26fd6b31c03dcbd2c410bc1a143af4
DDfire <developerdd@ukr.net>
2019-11-19 16:58:44 +0200

    вариант В №1-12

---
+++ src/HomeWorkForBook.java
@@ -0,0 +1,53 @@
+public class HomeWorkForBook {
+    //Числа Фибоначчи: f0 = f1 = 1, f (n) = f (n–1) + f (n–2)
+    public static int Fibo(int index) {
+        if (index <= 0) {
+            return 0;
+        } else if (index == 1) {
+            return 1;
+        } else {
+            return Fibo(index - 1) + Fibo(index - 2);
+        }
+    }
+
+    public static void ShowMas(int[] mas) {
+        for (int i :
+                mas) {
+            System.out.println(i);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] mas = new int[]{12, 21, 33, 4, 5, 6, 9, 27};
+        int max = 0;
+        int min = mas[0];
+        System.out.println("Числа которые делять на 3 или 9 :");
+        for (int i :
+                mas) {
+            if (i % 3 == 0 || i % 9 == 0) {
+                System.out.print(" " + i);
+            }
+            if (i > max) {
+                max = i;
+            }
+            if (i < min) {
+                min = i;
+            }
+        }
+        System.out.print("\n" + "Max = " + max + "\n" + "Min = " + min);
+        int tamp;
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length - 1; j++) {
+                if (mas[i] < mas[j]) {     //смена знака влияет на направление сортировки
+                    tamp = mas[i];
+                    mas[i] = mas[j];
+                    mas[j] = tamp;
+                }
+            }
+        }
+        int numberFibo = 5;
+
+        System.out.println(Fibo(numberFibo));
+    }
+}
