a162e58a129a087a8483cff5d47ba29c3e16d6bf
DDfire <developerdd@ukr.net>
2019-11-23 19:37:55 +0200

    создал класс Matrix

---
+++ src/Matrix.java
@@ -0,0 +1,38 @@
+public class Matrix {
+    int [][] mas;
+
+    public Matrix(int row,int kolum) {
+        mas = new int[row][kolum];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < kolum; j++) {
+                setElemetsMatrix(i,j,(int)(Math.random()*kolum*kolum));
+            }
+        }
+    }
+
+    public int getVerticalSize(){
+        return mas.length;
+    }
+    public int getHorizonyalSixe(){
+        return mas[0].length;
+    }
+
+    public int getElemetsMatrix(int row,int kolum) {
+        return mas[row][kolum];
+    }
+
+    public void setElemetsMatrix(int row,int kolum,int value) {
+        mas[row][kolum]=value;
+    }
+
+    public String toSting(){
+        String str= "Matrix : "+mas.length+" x"+mas[0].length+"\n";
+        for (int[] vector:mas) {
+            for (int value:vector) {
+                str+=value+" ";
+            }
+            str+="\n";
+        }
+        return str;
+    }
+}
