641e81e7207af03f594380c97fa37e549718380e
ddfire <developerDD@ukr.net>
2020-02-15 21:00:14 +0200

    CodeWars Good idea done

--- src/HomeWorkForBook.java
+++ src/HomeWorkForBook.java
@@ -14,6 +14,12 @@ public class HomeWorkForBook {
     public static void main(String[] args){
         int[]ar={-5,6,-10};
         System.out.println(MinInArray.findSmallestInt(ar));
+        String[] str = new String[] {"good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"};
+        String[] st1r = new String[] {"bad", "bad", "bad", "bad"};
+        String[] st2r = new String[] {"good", "bad", "bad", "bad", "bad", "bad", "bad", "good"};
+        System.out.println(MinInArray.well(str));
+        System.out.println(MinInArray.well(st1r));
+        System.out.println(MinInArray.well(st2r));
 
 
 
--- src/com/CodeWars/MinInArray.java
+++ src/com/CodeWars/MinInArray.java
@@ -7,4 +7,21 @@ public class MinInArray {
         Arrays.sort(args);
         return args[0];
     }
+    public static String well(String[] x){
+        int flag=0;
+        for(String str: x){
+            if (str=="good"){
+                flag++;
+            }
+            if(flag==3){
+                return"I smell a series!";
+            }
+        }
+        if(flag==1||flag==2){
+            return"Publish!";
+        }else{
+            return"Fail!";
+        }
+    }
 }
+
