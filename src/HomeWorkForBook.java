0d5f45bd06df35774bbdd66165cd24ed012586c3
ddfire <developerDD@ukr.net>
2020-02-25 22:06:43 +0200

    CodeWars Convert integer to Whitespace format done fix

--- src/HomeWorkForBook.java
+++ src/HomeWorkForBook.java
@@ -14,39 +14,7 @@ public class HomeWorkForBook {
     public static void main(String[] args) {
         //int k = 2;
        // System.out.println(k << 3 >> k == k >> 1 << k);//разобраться
-        int k = 0;
-        int temp = k;
-        StringBuilder stringBuilder = new StringBuilder();
-        StringBuilder stringBuilder1 = new StringBuilder();
-        if (k<0){
-            k=k*(-1);
-         }
-        if (k==0){
-            stringBuilder1.replace(0,1," ");
-        }else{
-            while (k!=0){
-                stringBuilder.append(k%2);
-                k/=2;
-            }
-            stringBuilder.reverse();
 
-            if (temp<0){
-                stringBuilder1.append('\t');//\t
-            }else {
-                stringBuilder1.append(' ');//\space
-            }
-            for (int i = 0; i < stringBuilder.length(); i++) {
-                if (stringBuilder.charAt(i)=='0'){
-                    stringBuilder1.append(' ');
-                }
-                if (stringBuilder.charAt(i)=='1'){
-                    stringBuilder1.append('\t');
-                }
-
-            }
-        }
-        stringBuilder1.append('\n');
-        System.out.println(stringBuilder1.toString());
 
     }
 
