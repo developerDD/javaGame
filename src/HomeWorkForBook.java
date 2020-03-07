dd33e2bad265745521658d6e74773435cc625e57
ddfire <developerDD@ukr.net>
2020-03-07 15:39:27 +0200

    CodeWars Easy Diagonal Pascals` Triangle done

--- src/HomeWorkForBook.java
+++ src/HomeWorkForBook.java
@@ -22,7 +22,8 @@ public class HomeWorkForBook {
         //System.out.println(countOddPositionChars(new String[]{"Hello","world","from", "array"}));
         //System.out.println(hasNoneLetters("fabcde", "fgh"));
        //FromTheMinimum.minValue(new int[]{6, 7, 7, 7, 6, 6});
-        EasyDiagonalPascalsTriangele.easyLine(20,5);
+        EasyDiagonalPascalsTriangele.easyLine(20,3);
+
     }
     public static int sdd(String input){
         if (input==null||input.isEmpty()){
--- src/com/CodeWars/EasyDiagonalPascalsTriangele.java
+++ src/com/CodeWars/EasyDiagonalPascalsTriangele.java
@@ -6,27 +6,39 @@ import java.util.Arrays;
 
 public class EasyDiagonalPascalsTriangele {
     public static BigInteger easyLine(int n, int p) {
-        // your code
-        BigInteger sum = BigInteger.valueOf(0);
-        ArrayList<BigInteger> cerrent = new ArrayList<>();
-        ArrayList<BigInteger> previous = new ArrayList<>(n);
-        BigInteger firstLast = BigInteger.valueOf(1);
-        int diagonal = p;
-        for (int i = 1; i <= n + 1; i++) {
-            for (int j = 0, index = 0; j < i; j++, index++) {
-                if (j == 0 || j == i - 1) {
-                    cerrent.add(firstLast);
-                } else {
-                    cerrent.add(previous.get(index).add(previous.get(index - 1)));
-                }
-                if (j == diagonal) {
-                    sum = sum.add(cerrent.get(j));
-                }
+        // my code
+//        BigInteger sum = BigInteger.valueOf(0);
+//        ArrayList<BigInteger> cerrent = new ArrayList<>();
+//        ArrayList<BigInteger> previous = new ArrayList<>(n);
+//        BigInteger firstLast = BigInteger.valueOf(1);
+//        int diagonal = p;
+//        for (int i = 1; i <= n + 1; i++) {
+//            for (int j = 0, index = 0; j < i; j++, index++) {
+//                if (j == 0 || j == i - 1) {
+//                    cerrent.add(firstLast);
+//                } else {
+//                    cerrent.add(previous.get(index).add(previous.get(index - 1)));
+//                }
+//                if (j == diagonal) {
+//                    sum = sum.add(cerrent.get(j));
+//                }
+//            }
+//            previous.clear();
+//            previous.addAll(0, cerrent);
+//            cerrent.clear();
+//        }
+//        return sum;
+
+        BigInteger findNumber = BigInteger.ONE;
+        n++;
+        for(int i=1; i<=n+1; i++) {
+            //строка триугольника которая находиться ниже заданой, так как сумма диагонали указаной в р
+            // это то число которое находиться от диться на нижней строчки триугольника с права от диагонали
+            findNumber = (findNumber.multiply(BigInteger.valueOf(n+1-i))).divide(BigInteger.valueOf(i));
+            if (i==p+1){
+                break;
             }
-            previous.clear();
-            previous.addAll(0, cerrent);
-            cerrent.clear();
         }
-        return sum;
+        return findNumber;
     }
 }
