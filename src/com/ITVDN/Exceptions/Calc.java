9dfa1b86009ff9776c56c9014e4066f046e08a48
ddfire <developerDD@ukr.net>
2020-04-11 19:23:15 +0300

    ITVDN class Calc vresion 1.1

--- src/com/ITVDN/Exceptions/Calc.java
+++ src/com/ITVDN/Exceptions/Calc.java
@@ -1,56 +1,79 @@
 package com.ITVDN.Exceptions;
 
-import org.jetbrains.annotations.NotNull;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Calc {
     private int a;
     private int b;
     private int result;
+    private String operator;
     boolean isOk;
+
+    public Calc(){
+
+    }
 
     public Calc(int a, int b) {
         this.a = a;
         this.b = b;
     }
 
-    public int getResult() {
-        return result;
+    private void inputData(){
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Введите первый опереанд: ");
+            a=sc.nextInt();
+            System.out.print("Введите второй опереанд: ");
+            b=sc.nextInt();
+            System.out.print("Введите + - * / : ");
+            operator=sc.next();
+        } catch (InputMismatchException e) {
+            System.out.println("Введено не число!");
+        }
     }
 
-    public boolean isOk() {
-        return isOk;
-    }
-
-    public void OperationCalc(@NotNull String op) throws ArithmeticException {
-        switch (op) {
-            case "+":
-                result = a + b;
-                isOk=true;
-                break;
-            case "-":
-                result = a-b;
-                isOk=true;
-                break;
-            case "*":
-                result = a * b;
-                isOk=true;
-                break;
-            case "/":
-                //1
-                // if (b==0)  throw new ArithmeticException();
-                //2
-                try {
-                    result = a/b;
+    private void OperationCalc() {
+        try {
+            switch (operator) {
+                case "+":
+                    result = a + b;
                     isOk=true;
-                } catch (Exception e) {
-                    //System.out.println("/ by zero!!!");
-                    //получение информации об исключении
-                    System.out.println(e.getMessage());// конкретное какое
-                    System.out.println(e.getClass().getName());// тип исключениея
-                }
-                break;
-            default:
-                System.out.println("Error operation!");
+                    break;
+                case "-":
+                    result = a-b;
+                    isOk=true;
+                    break;
+                case "*":
+                    result = a * b;
+                    isOk=true;
+                    break;
+                case "/":
+                    //1
+                    // if (b==0)  throw new ArithmeticException();
+                    //2
+                    try {
+                        result = a/b;
+                        isOk=true;
+                    } catch (Exception e) {
+                        //System.out.println("/ by zero!!!");
+                        //получение информации об исключении
+                        System.out.println(e.getMessage());// конкретное какое
+                        System.out.println(e.getClass().getName());// тип исключениея
+                    }
+                    break;
+                default:
+                    System.out.println("Error operation!");
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public void Action(){
+        inputData();
+        OperationCalc();
+        if (isOk){
+            System.out.println(a+operator+b+" = " + result);
         }
     }
 }
--- src/com/ITVDN/Exceptions/Main.java
+++ src/com/ITVDN/Exceptions/Main.java
@@ -1,31 +1,30 @@
 package com.ITVDN.Exceptions;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int a = 0;
-        int b = 0;
-        String op;
-        Calc calc = null;
-        Scanner sc = new Scanner(System.in);
-        try {
-            a = sc.nextInt();
-            b = sc.nextInt();
-            op = sc.next();
-            calc = new Calc(a, b);
-            calc.OperationCalc(op);
-            if (calc.isOk) {
-                System.out.println(calc.getResult());
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println(e.getClass().getName());
-        } finally {
-            sc.close();
-        }
-
-
+//        int a;
+//        int b;
+//        String op;
+//        Calc calc;
+//        Scanner sc = new Scanner(System.in);
+//        try {
+//            a = sc.nextInt();
+//            b = sc.nextInt();
+//            op = sc.next();
+//            calc = new Calc(a, b);
+//            calc.OperationCalc(op);
+//            if (calc.isOk) {
+//                System.out.println(calc.getResult());
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            System.out.println(e.getCause());
+//            System.out.println(e.getClass().getName());
+//        } finally {
+//            sc.close();
+//        }
+        Calc c1 = new Calc();
+        c1.Action();
 
     }
 }
