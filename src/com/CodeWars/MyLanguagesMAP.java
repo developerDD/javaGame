40cf86a7669002f99bfb44385b84de955d289a01
ddfire <developerDD@ukr.net>
2020-04-18 15:06:30 +0300

    Code Wars My languages v2

--- src/com/CodeWars/MyLanguagesMAP.java
+++ src/com/CodeWars/MyLanguagesMAP.java
@@ -4,17 +4,25 @@ import java.util.*;
 
 public class MyLanguagesMAP {
     public static List<String> myLanguages(final Map<String, Integer> results) {
-        TreeMap treeMap = new TreeMap(results);
-        Object[] strings = treeMap.keySet().toArray();
-        Object[] collection = treeMap.values().toArray();
-        List<String > languages = new ArrayList<>();
-        for (int i = strings.length-1; i >=0 ; i--) {
-            if((int)collection[i]>=60){
-                languages.add((String) strings[i]);
-            }else {
-                break;
+        TreeMap treeMap = new TreeMap();
+        Object[] key = results.keySet().toArray();
+        Object[] value = results.values().toArray();
+        List<String> languages = new ArrayList<>();
+        for (int i = key.length-1; i >=0; i--) {
+            treeMap.put(value[i], key[i]);
+        }
+
+        key = treeMap.keySet().toArray();
+        System.out.println(Arrays.toString(key));
+        value=treeMap.values().toArray();
+        System.out.println(Arrays.toString(value));
+
+        for (int i = key.length-1; i >=0 ; i--) {
+            if ((int)key[i]>=60){
+                languages.add((String) value[i]);
             }
         }
+
         return languages;
     }
 }
